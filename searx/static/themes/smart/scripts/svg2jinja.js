d977eb36414aba4725553b62049daf3d7ab0fd75
Gnkalk <Gnkalk@yahoo.com>
2025-01-01 18:57:49 +0330

    start templating result page

--- searx/static/themes/smart/scripts/svg2jinja.js
+++ searx/static/themes/smart/scripts/svg2jinja.js
@@ -5,6 +5,7 @@ const svgList = {
   images: "node_modules/remixicon/icons/Media/image-fill.svg",
   videos: "node_modules/remixicon/icons/Media/video-fill.svg",
   news: "node_modules/remixicon/icons/Document/newspaper-fill.svg",
+  music: "node_modules/remixicon/icons/Media/music-2-fill.svg",
   search: "node_modules/remixicon/icons/System/search-line.svg",
 };
 
